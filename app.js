df7899bdca56feb23297863e1db1b5d229847ece
Elee Lawleit <alibutt200235@gmail.com>
2022-08-07 16:46:07 +0500

    Waiting for twitter API Key

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const TWITTER_CONSUMER_KEY = "22186bdcbbceb97607c865d47aa8b8994893808333212f21d4b726949bb55414e036d2d54c05742c0776593795838c1d5b95cc02f77ff5ac96b1f6def858ba48";
+const TWITTER_CONSUMER_SECRET = "e3931af72805e9efd6de0dedef36e4902f82f37a2003e073c4c0c0bb4a8be95929ce9302265f463ce13c82bd68c22d5c635e67b6aad0a79daa10e81a5c925441";
+
 const express = require("express");
 const path = require("path");
 const http = require("http");
@@ -5,11 +8,37 @@ const http = require("http");
 const session = require("express-session");
 const { MongoClient } = require("mongodb");
 
+
+
 //route files
 const indexFile = require("./routes/index");
 const articleFile = require("./routes/article");
 const userFile = require("./routes/user");
 
+//for twitter signin
+//this will work only when I have to access to the twitter API key :"), going to push it nonetheless
+const everyauth = require("everyauth");
+everyauth.debug = true;
+everyauth.twitter
+    .consumerKey(TWITTER_CONSUMER_KEY)
+    .consumerSecret(TWITTER_CONSUMER_SECRET)
+    .findOrCreateUser((session, accessToken, accessTokenSecret, twitterUserMetadata)=>{
+        const promise = this.Promise();
+        process.nextTick(()=>{
+            if(twitterUserMetadata.screen_name === "ELawleit"){
+                session.user = twitterUserMetadata;
+                session.admin = true;
+            }
+            promise.fulfil(twitterUserMetadata);
+        });
+        return promise;
+    }).redirectPath("/admin");
+
+//to make sure the user is logged out
+everyauth.everymodule.handleLogout(userFile.logout);
+everyauth.everymodule.findUserById((user, callback)=>{
+    callback(user);
+});
 
 //doing database-y things
 const url = "mongodb://localhost:27017";
@@ -62,6 +91,8 @@ app.use(express.urlencoded({ extended: true }));
 //for cookies and sessions
 // app.use(cookieParser('3CCC4ACD-6ED1-4844-9217-82131BDCB239'));
 app.use(session({ secret: 'sf65165  iuhhibuy67bW#@^##@t rvbf7 ff75f6511348986753t r#@@&#&EGDJH%%#Eh', resave: true, saveUninitialized: true }));
+
+app.use(everyauth.middleware());
 
 //authentication middlewate
 const authorize = (req, res, next) => {
